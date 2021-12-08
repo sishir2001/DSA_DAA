7df3d98e3127a0bf550f69feb2b845a70036656a
Jaladi Saai Sishir <54372985+sishir2001@users.noreply.github.com>
2021-12-09 03:43:46 +0530

    Updation of template

--- CompetitiveTemplate.cpp
+++ CompetitiveTemplate.cpp
@@ -1,11 +1,19 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 typedef long long ll;
+// typedef vector<ll> v_ll;
+
+#define mod 1000000007 // 1e9+7
+#define deb(x) cout << #x << " = "<<x<<"\n" // for debugging
+#define deb2(x,y) cout << #x << " = "<<x << " , " << #y << " = " << y <<"\n" // for debugging 
+#define FOR(a,n) for(int (a) = 0;(a) < (n);(a)++) // regular for loop
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
     int T;
     cin >> T;
     while(T){
