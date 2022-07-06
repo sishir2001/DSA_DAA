2dd21cd5295b4494d4505d71b3adb9c6bd360ea2
Jaladi Saai Sishir <54372985+sishir2001@users.noreply.github.com>
2022-07-06 12:48:21 +0530

    timely commit

--- Trees/build_tree.cpp
+++ Trees/build_tree.cpp
@@ -39,8 +39,14 @@ typedef pair<uint, uint> p_uint;
 
 class Node;
 
-Node *build_tree(v_ll &nums, int *i);
+// preorder build
+Node *preorder_build(v_ll &nums, int *i);
+Node *levelorder_build(v_ll &nums);
 void preorder_traversal(Node *root);
+void inorder_traversal(Node *root);
+void postorder_traversal(Node *root);
+void levelorder_traversal(Node *root);
+int height_of_tree(Node *root);
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -56,10 +62,7 @@ int main() {
         cin >> n;
         v_ll nums(n);  // vector
         FOR(i, n, 0) { cin >> nums[i]; }
-        int x = 0, *i;
-        i = &x;
-        Node *root = build_tree(nums, i);
-        preorder_traversal(root);
+        Node *root = levelorder_build(nums);
         nums.clear();
         T--;
     }
@@ -76,25 +79,99 @@ class Node {
     }
 };
 
-Node *build_tree(v_ll &nums, int *i) {
+Node *preorder_build(v_ll &nums, int *i) {
     // * preorder build of a tree root-left-right
     if (*i == nums.size() || nums[*i] == -1)
         return NULL;
     Node *root = new Node(nums[*i]);
     *i += 1;
-    root->left = build_tree(nums, i);
+    root->left = preorder_build(nums, i);
     *i += 1;
-    root->right = build_tree(nums, i);
+    root->right = preorder_build(nums, i);
     return root;
 }
 
 void preorder_traversal(Node *root) {
+    // * left root right
     if (!root) {
-        cout << "-1"
-             << " ";
         return;
     }
     cout << root->data << " ";
     preorder_traversal(root->left);
     preorder_traversal(root->right);
+}
+
+void inorder_traversal(Node *root) {
+    // * left root right
+    if (!root) {
+        return;
+    }
+    inorder_traversal(root->left);
+    cout << root->data << " ";
+    inorder_traversal(root->right);
+}
+
+void postorder_traversal(Node *root) {
+    // * left right root
+    if (!root)
+        return;
+    postorder_traversal(root->left);
+    postorder_traversal(root->right);
+    cout << root->data << " ";
+}
+
+void levelorder_traversal(Node *root) {
+    // * we need breadth search
+    // we iterate over a queue
+    if (!root)
+        return;
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty()) {
+        // pushing the children of the front of queue
+        if (!q.front()) {
+            q.pop();
+            NXT_LINE;
+            if (!q.empty())
+                q.push(NULL);
+            continue;
+        }
+        Node *front = q.front();
+        q.pop();
+        if (front->left)
+            q.push(front->left);
+        if (front->right)
+            q.push(front->right);
+        cout << front->data << " ";
+    }
+}
+
+Node *levelorder_build(v_ll &nums) {
+    // iterative approach using queue
+    if (nums.size() == 0 || nums[0] == -1)
+        return NULL;
+    int i = 0;
+    Node *root = new Node(nums[0]);
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty()) {
+        Node *curr = q.front();
+        i++;
+        curr->left = nums[i] == -1 ? NULL : new Node(nums[i]);
+        i++;
+        curr->right = nums[i] == -1 ? NULL : new Node(nums[i]);
+        if (curr->left)
+            q.push(curr->left);
+        if (curr->right)
+            q.push(curr->right);
+        q.pop();
+    }
+    return root;
+}
+
+int height_of_tree(Node *root) {
+    if (!root)
+        return 0;
+    return max(height_of_tree(root->left), height_of_tree(root->right)) + 1;
 }
